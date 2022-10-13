3eec29846bb612269c135ef0a655ab44363ac961
sapux05 clouddnagmbhgmail.onmicrosoft.com <sapux05@clouddnagmbhgmail.onmicrosoft.com>
2022-10-13 08:20:09 +0000

    Formatter added

--- webapp/controller/Customer.controller.js
+++ webapp/controller/Customer.controller.js
@@ -10,6 +10,15 @@ sap.ui.define([
         return Controller.extend("at.clouddna.student05.zhoui5.controller.Customer", {
             onInit: function () {
 
+            },
+
+            genderFormatter: function(sKey){ //s for string, a for array, o - Object, i - init.....
+                let oView = this.getView(); //auf zugehörige View zum Controller zugreifen
+                let oI18nModel = oView.getModel("i18n"); //Modename aus manifest
+                let oResourceBundle = oI18nModel.getResourceBundle(); //Texte aus ResourceBundle lesen zwecks Übersetzung
+                let sText = oResourceBundle.getText(sKey); //ist Schlüssel im i18n verfügbar, wird er gelesen
+                
+                return sText; 
             }
         });
     });
